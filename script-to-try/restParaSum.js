fccf9d146166acd8e09846df079289353744ed40
nasuda <nasuda908@gmail.com>
2024-11-14 05:11:50 +0900

    add scripts

---
+++ script-to-try/restParaSum.js
@@ -0,0 +1,6 @@
+function sum(...args) {
+  args.forEach(arg => console.log(arg))
+}
+
+
+sum(1, 2, 3);
---
+++ script-to-try/sum.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+  const total = a + b;
+  console.log(total);
+}
+
+
+sum(8, 9);
